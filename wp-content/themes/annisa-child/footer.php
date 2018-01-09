b3b7b6f2ea66ce7f6e039f63162f262fbd4b08db
smontoro <sarahjmontoro@gmail.com>
2018-01-09 01:01:13 -0800

    gave footer new styles

--- wp-content/themes/annisa-child/footer.php
+++ wp-content/themes/annisa-child/footer.php
@@ -8,7 +8,7 @@
 ?>
 
 
-<footer id="colophon" class="py-5" role="contentinfo">
+<footer id="colophon" role="contentinfo">
   <?php if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) : ?>
 
     <hr>
@@ -16,7 +16,7 @@
 
  <!-- SIGN UP
     =========================================-->
-    <section class="container">
+    <section class="container mt-5">
       <div class="row">
           <div class="widget-area col text-center">
            <?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
@@ -24,12 +24,53 @@
             <?php endif; ?>
           </div>
       </div>
-          <!-- .widget-area -->
+      <div class="text-center">
+
+        <hr class="footer-hr">
+
+      <ul id="footer-social">
+        <li>
+          <a href="https://www.facebook.com/enterpriseforyouth/" target="_blank">
+            <i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/enterprise_youth/" target="_blank">
+            <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/EFY_SF" target="_blank">
+            <i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/company/81612/" target="_blank">
+            <i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+
     </section>
 
 
-    
-
+ <!-- ENTERPRISE
+    =========================================--> 
+    <section class="container-fluid">
+      <div class="row text-center">
+          <div class="col-sm-6 mt-4">
+           <?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
+            <?php dynamic_sidebar( 'footer-2' ); ?>
+            <?php endif; ?>
+          </div>
+          <div class="col-sm-6 logo mt-2">
+           <?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
+            <?php dynamic_sidebar( 'footer-3' ); ?>
+            <?php endif; ?>
+          </div>
+      </div>
+    </section>
 
 
   <?php endif; ?>
