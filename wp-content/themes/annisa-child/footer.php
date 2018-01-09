c9c26e9e3a49e22652bf838497b72ad3ed5a7916
smontoro <sarahjmontoro@gmail.com>
2018-01-08 23:46:59 -0800

    added extra row to footer

--- wp-content/themes/annisa-child/footer.php
+++ wp-content/themes/annisa-child/footer.php
@@ -8,41 +8,32 @@
 ?>
 
 
-<footer id="colophon" class="py-5 container-fluid" role="contentinfo">
+<footer id="colophon" class="py-5" role="contentinfo">
   <?php if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) : ?>
 
-    <div class="row text-white mx-auto">
+    <hr>
+    
 
-    <!-- CONTACT
-    ========================================-->                 
-          <div class="widget-area col-sm-4 text-center">
-            <?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+ <!-- SIGN UP
+    =========================================-->
+    <section class="container">
+      <div class="row">
+          <div class="widget-area col text-center">
+           <?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
             <?php dynamic_sidebar( 'footer-1' ); ?>
             <?php endif; ?>
           </div>
+      </div>
           <!-- .widget-area -->
+    </section>
 
-    <!-- SIGN UP
-    =========================================-->
-          <div class="widget-area col-sm-4">
-            <div class="text-center">
-           <?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-            <?php dynamic_sidebar( 'footer-2' ); ?>
-            <?php endif; ?>
-          </div>
-          </div>
-          <!-- .widget-area -->
-          
-    <!-- LOGO
-    =========================================-->
-          <div class="widget-area col-sm-4 mt-5 text-center">
-            <img src="http://sfprivatecollections.org/wp-content/uploads/2017/05/enterprise-footer.png">
-          </div>
-          <!-- .widget-area --> 
-  
+
+    
+
+
+
   <?php endif; ?>
- 
-  </div><!--row-->
+
 </footer>
 
 <?php wp_footer(); ?>
