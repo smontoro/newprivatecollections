06bd436b884b65e25da6bbf935fbdc5fe4bc091d
smontoro <sarahjmontoro@gmail.com>
2018-01-11 22:44:45 -0800

    fixed favicon bug

--- wp-content/themes/annisa-child/header.php
+++ wp-content/themes/annisa-child/header.php
@@ -22,7 +22,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	
 	<!-- Tab logo -->
-    <link rel="shortcut icon" href="http://sfprivatecollections.org/wp-content/uploads/2018/01/favicon.png"/>
+    <link rel="shortcut icon" href="http://sfprivatecollections.org/wp-content/uploads/2018/01/favicon.png" type="image/x-icon"/>
 
 	<?php wp_head(); ?>
 </head>
