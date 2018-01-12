0ca85900d07de10df54d1cf5856a736462bc78ef
smontoro <sarahjmontoro@gmail.com>
2018-01-11 22:48:20 -0800

    removed favicon

--- wp-content/themes/annisa-child/header.php
+++ wp-content/themes/annisa-child/header.php
@@ -20,9 +20,6 @@
 	<!--Bootstrap Styles-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-	
-	<!-- Tab logo -->
-    <link rel="shortcut icon" href="http://sfprivatecollections.org/wp-content/uploads/2018/01/favicon.png" type="image/x-icon"/>
 
 	<?php wp_head(); ?>
 </head>
